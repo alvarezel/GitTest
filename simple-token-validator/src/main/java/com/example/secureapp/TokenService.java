688488f9aea9a4e35c7428a4965412a0c94f336d
alvarezel <alvarezellysa@gmail.com>
2026-05-15 23:43:07 -0700

    test

--- simple-token-validator/src/main/java/com/example/secureapp/TokenService.java
+++ simple-token-validator/src/main/java/com/example/secureapp/TokenService.java
@@ -6,7 +6,7 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
-#test
+//test
 public class TokenService {
 
     private static final String SECRET_KEY = System.getenv("TOKEN_SECRET") != null 
